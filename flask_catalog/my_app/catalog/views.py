b07c21fe940649301b818e8663cc86d72003f2cc
Paul Davis <pjdavis@gmx.com>
2017-10-06 11:10:05 +0100

    Update views.py

--- flask_catalog/my_app/catalog/views.py
+++ flask_catalog/my_app/catalog/views.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, Blueprint
 from my_app import app, db
-from my_app.catalog.models import Product
+from my_app.catalog.models import Product, Category
 
 
 catalog = Blueprint('catalog', __name__)
@@ -19,7 +19,8 @@ def products():
     for product in products:
         res[product.id] = {
             'name': product.name,
-            'price': str(product.price)
+            'price': str(product.price),
+            'category': product.category.name
                             }
     return jsonify(res)
 
@@ -28,9 +29,31 @@ def products():
 def create_product():
     name = request.form.get('name')
     price = request.form.get('price')
-    product = Product(name, price)
+    categ_name = request.form.get('category')
+    category = Category.query.filter_by(name=categ_name).first()
+    if not category:
+        category = Category(categ_name)
+    product = Product(name, price, category)
     db.session.add(product)
     db.session.commit()
     return 'Product created.'
 
 
+@catalog.route('/category-create', methods=['POST',])
+def create_category():
+    name = request.form.get('name')
+    category = Category(name)
+    db.session.add(category)
+    db.session.commit()
+    return 'Category created.'
+
+
+@catalog.route('/categories')
+def categories():
+    categories = Category.query.all()
+    res = {}
+    for category in categories:
+        res[category.id] = {'name': category.name}
+    for product in category.products:
+        res[category.id]['products'] = {'id': product.id, 'name': product.name, 'price': product.price}
+    return jsonify(res)
