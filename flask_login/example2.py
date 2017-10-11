59f9fbf995b30fba625683c6e106562438a3215b
Paul Davis <pjdavis@gmx.com>
2017-10-11 15:37:35 +0100

    Update example2.py

--- flask_login/example2.py
+++ flask_login/example2.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
@@ -10,8 +10,15 @@ app.config['SECRET_KEY'] = 'thisissecret'
 
 db = SQLAlchemy(app)
 
+#instantiate flask_login
 login_manager = LoginManager()
+#initializes the instance
 login_manager.init_app(app)
+#send un-logged in user to login page instead of 'Unauthorized' for protected page(s)
+login_manager.login_view = 'login'
+#for 'get_flashed_messages' in login.html
+login_manager.login_message = 'You need to login'
+
 
 
 class User(UserMixin, db.Model):
@@ -25,10 +32,18 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@app.route('/')
-def index():
-    # in a real example, would usually authenticate user before the following
-    user = User.query.filter_by(username='Paul').first()
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+# <form action="/logmein' method="POST"> within login.html redirects user to this route:
+@app.route('/logmein', methods=['POST'])
+def logmein():
+    username = request.form['username']
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return '<h1>User not found!</h1>'
     login_user(user)
     return 'You are now logged in!'
 
@@ -48,5 +63,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
