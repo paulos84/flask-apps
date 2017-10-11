a79e1b67808676fa6bc615a5b3bddd9d12110160
Paul Davis <pjdavis@gmx.com>
2017-10-11 11:05:36 +0100

    Update login_example.py

--- flask_login/login_example.py
+++ flask_login/login_example.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 
 app = Flask(__name__)
 
@@ -13,11 +13,39 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True)
+
 
 #this function allows flask_login to connects a User instance with a user that flask_login handles
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+@app.route('/')
+def index():
+    # in a real example, would usually authenticate user before the following
+    user = User.query.filter_by(username='Paul').first()
+    login_user(user)
+    return 'You are now logged in!'
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'You are now logged out!'
+
+
+@app.route('/home')
+@login_required
+def home():
+    return 'The current user is: ' + current_user.username
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
