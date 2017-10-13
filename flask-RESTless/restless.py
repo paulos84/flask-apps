cb9a338623bda64865799ede4ca79b78afc0af68
Paul Davis <pjdavis@gmx.com>
2017-10-13 09:55:23 +0100

    Update restless.py

--- flask-RESTless/restless.py
+++ flask-RESTless/restless.py
@@ -1,1 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.restless import APIManager
 
+
+app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    pets = db.relationship('Pet', backref='owner', lazy='dynamic')
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+if __name__ == '__main__':
+    app.run()
