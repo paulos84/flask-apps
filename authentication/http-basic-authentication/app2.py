b6c04e535169cb770d2b4df20faa2f5d86908986
Paul Davis <pjdavis@gmx.com>
2017-10-15 16:56:32 +0100

    update app2

--- authentication/http-basic-authentication/app2.py
+++ authentication/http-basic-authentication/app2.py
@@ -1,6 +1,17 @@
 from flask import Flask, request, make_response
+from functools import wraps
 
 app = Flask(__name__)
+
+#create a decorators required user to be logged in
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if auth and auth.username == 'username' and auth.password == 'password':
+            return f(*args, **kwargs)
+        return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    return decorated
 
 @app.route('/')
 def index():
@@ -9,11 +20,11 @@ def index():
     #the 3rd arguments tells browser that a login is required
     return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
-#only index page requires login, i.e. /page is not protected
 
 @app.route('/page')
+@auth_required
 def page():
-    return 'you are logged in'
+    return 'you are on the page'
 
 
 if __name__ == '__main__':
