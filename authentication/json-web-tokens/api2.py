0419e161b4485ceff062535187044a53fc6739d3
Paul Davis <pjdavis@gmx.com>
2017-10-16 16:01:36 +0100

    Update api2.py

--- authentication/json-web-tokens/api2.py
+++ authentication/json-web-tokens/api2.py
@@ -1,5 +1,5 @@
 
-#Authenticating an API using flask-SQLAlchemy and JSON Web Tokens
+#Authenticating an API using JSON Web Tokens
 
 from flask import Flask, request, make_response, jsonify
 import jwt
@@ -15,5 +15,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(50))
+    complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer)
+    
+@app.route('/user', methods=['GET'])
+def get_all_users():
+    return ''
+
+@app.route('/user/<user_id>', methods=['POST'])
+def create_user():
+    return ''
+
+@app.route('/user/<user_id>', methods=['PUT'])
+def promote_user():
+    return ''
+
+@app.route('/user/<user_id>', methods=['DELETE'])
+def delete_user():
+    return ''
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=Trsue)
