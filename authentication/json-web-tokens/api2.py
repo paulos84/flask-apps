06284f9bef1a9e55ff7bc854f25292e7f915fd48
Paul Davis <pjdavis@gmx.com>
2017-10-17 09:56:55 +0100

    Update api2.py

--- authentication/json-web-tokens/api2.py
+++ authentication/json-web-tokens/api2.py
@@ -9,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thesecretkey'
@@ -17,6 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
@@ -24,11 +26,13 @@ class User(db.Model):
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer)
+    public_id = db.Column(db.Integer)
+
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
@@ -39,12 +43,22 @@ def get_all_users():
         user_data['public_id'] = user.public_id
         user_data['name'] = user.name
         user_data['password'] = user.password
+        user_data['admin'] = user.admin
         output.append(user_data)
-    return jsonify(output)
+    return jsonify({'users': output})
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_users():
-    return ''
+
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_users(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({'message': 'no user found'})
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    return jsonify({'user': user_data})
+
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -55,13 +69,16 @@ def create_user():
     db.session.commit()
     return jsonify({'message': 'new user created'})
 
-@app.route('/user/<user_id>', methods=['PUT'])
+
+@app.route('/user/<public_id>', methods=['PUT'])
 def promote_user():
     return ''
 
-@app.route('/user/<user_id>', methods=['DELETE'])
+
+@app.route('/user/<public_id>', methods=['DELETE'])
 def delete_user():
     return ''
+
 
 if __name__ == '__main__':
     app.run(debug=True)
