e6842e836f79317aa2204445edc2f9b9ae61f7db
Paul Davis <pjdavis@gmx.com>
2017-10-18 09:12:26 +0100

    Update api.py

--- flask-restful/cities/api.py
+++ flask-restful/cities/api.py
@@ -1,1 +1,29 @@
+from flask import Flask, Blueprint, jsonify
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SECRET_KEY'] = 'thesecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    age = db.Column(db.Integer(50))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
