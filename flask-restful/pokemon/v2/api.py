5cc602898f1eec6ea50eb37fcc6cafa5af3c6ed5
Paul Davis <pjdavis@gmx.com>
2017-10-24 09:48:16 +0100

    Update api.py

--- flask-restful/pokemon/v2/api.py
+++ flask-restful/pokemon/v2/api.py
@@ -1,30 +1,24 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 
-
 app = Flask(__name__)
 api = Api(app)
-
 
 pokedex = [{
     'number': '14',
     'name': 'Kakuna',
     'type': ['bug', 'poison'],
     'weaknesses': ['fire', 'flying', 'psychic', 'rock'],
-    #'evolutions': [{'number': 15, 'name': 'beedrill'}]
 }, {
     'number': '16',
     'name': 'Pidgey',
     'type': ['normal', 'flying'],
     'weaknesses': ['electric', 'ice', 'rock'],
-    #'evolutions': [{'number': 17, 'name': 'Pidgeotto'},
-                 #  {'number': 18, 'name': 'Pidgeot'}]
 }, {
     'number': '43',
     'name': 'Dugtrio',
     'type': ['ground'],
     'weaknesses': ['grass', 'ice', 'water'],
-    #'evolutions': []
 }]
 
 
@@ -34,20 +28,17 @@ def list_of_strings(value):
         raise ValueError("List or strings required")
     return value
 
+
 def get_pokemon_args():
-    root_parser = reqparse.RequestParser()
-    root_parser.add_argument('name', required=True, type=str, location='json')
-    root_parser.add_argument('number', required=True, type=int, location='json')
-    root_parser.add_argument('type', required=True, type=list_of_strings, location='json')
-    root_parser.add_argument('weaknesses', required=True, type=list_of_strings, location='json')
-    return root_parser.parse_args(strict=True)
-    #for a in root_args.get('evolutions'):
-       # evo_parser = reqparse.RequestParser()
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', required=True, type=str, location='json')
+    parser.add_argument('number', required=True, type=int, location='json')
+    parser.add_argument('type', required=True, type=list_of_strings, location='json')
+    parser.add_argument('weaknesses', required=True, type=list_of_strings, location='json')
+    return parser.parse_args(strict=True)
 
 
 class Pokemon(Resource):
-
-
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('number', required=False, type=str, location='args')
@@ -60,10 +51,6 @@ class Pokemon(Resource):
             else:
                 return {}
         return pokedex
-
-    #def post(self):
-     #   parser = reqparse.RequestParser()
-      #  parser.add_argument
 
     def post(self):
         args = get_pokemon_args()
@@ -81,6 +68,25 @@ class Pokemon(Resource):
             return {}
         pokedex.append(new_pokemon)
         return pokedex[-1]
+
+    def put(self):
+        args = get_pokemon_args()
+
+        new_pokemon = {
+            'name': args['name'],
+            'number': args['number'],
+            'type': args['type'],
+            'weaknesses': args['weaknesses']
+        }
+
+        new_number = new_pokemon['number']
+        all_numbers = [a['number'] for a in pokedex]
+        if new_number not in all_numbers:
+            return {}
+        index = all_numbers.index(new_number)
+        pokedex[index] = new_pokemon
+        
+        return pokedex[index]
 
 
 api.add_resource(Pokemon, '/api/v1/pokemon')
