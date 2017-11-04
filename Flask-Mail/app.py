464cc962cbade8cf9f80cbe8b88b5a53a09c9f85
Paul Davis <pjdavis@gmx.com>
2017-11-04 22:17:21 +0000

    update app

--- Flask-Mail/app.py
+++ Flask-Mail/app.py
@@ -3,16 +3,7 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-app.config['TESTING'] = False
-app.config['MAIL_SERVER'] = 'localhost'
-app.config['MAIL_PORT'] = 25
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = None
-app.config['MAIL_PASSWORD'] = None
-app.config['MAIL_DEFAULT_SENDER'] = None
-app.config['MAIL_MAX_EMAILS'] = None
-app.config['MAIL_SUPPRESS_SEND'] = False
-app.config['MAIL_ASCII_ATTACHMENTS'] = False
+mail = Mail(app)
+
+#mail = Mail()
+#mail.init_app(app)
