e23be66b3e80ce0cbe9dbaf13ac79f6b6427f995
Paul Davis <pjdavis@gmx.com>
2017-11-06 17:14:02 +0000

    update app

--- Flask-Mail/app.py
+++ Flask-Mail/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
 
 mail = Mail(app)
 
@@ -12,7 +13,7 @@ mail = Mail(app)
 
 @app.route('/')
 def index():
-    msg = Message('Hello, how are you?', recipients=['pj_davis@hotmail.co.uk'])
+    msg = Message('Hello, how are you?', recipients=['@hotmail.co.uk'])
     msg.body = '<b>This is the body of the test email</b>'
     #msg.html = '<b>This is a test email</b>'
     mail.send(msg)
