0ae9579c9408072919415649078b3d1ab7c2b246
Paul Davis <pjdavis@gmx.com>
2017-11-16 09:05:04 +0000

    Update auth_required_decorator.py

--- auth_required_decorator.py
+++ auth_required_decorator.py
@@ -1,1 +1,32 @@
+from flask import Flask, request, Response
+from functools import wraps
 
+app = Flask(__name__)
+
+def check_auth(username, password):
+    # This function is called to check if a username password combination is valid.
+     return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n' 
+     'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/page')
+@auth_required
+def page():
+    return 'you are on the page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
