50ee4a8e705b456c8b5928d0837d22881df5a1f7
Paul Davis <pjdavis@gmx.com>
2017-12-09 10:41:30 +0100

    enable plot multiple sites and parameters

--- js-chart-libs/aurn-charts/app_2_sites.py
+++ js-chart-libs/aurn-charts/app_2_sites.py
@@ -8,13 +8,19 @@ app = Flask(__name__)
     # 2 sites and multiple pollutants
 
 
-def chart_data(site, days, p1, p2):
+def chart_data(site, days, *args, site2=None):
+    if site2:
+        url = 'http://www.air-aware.com:8083/data/{0}/{1}/{2}'.format(site, site2, days)
+
     url = 'http://www.air-aware.com:8083/data/{0}/{1}'.format(site, days)
     resp = requests.get(url).json()
     data = resp[site.upper()]['latest_data']
     times = [a['time'] for a in data]
-    s1 = ['' if a['values'][p1] in ['n/a', 'n/m'] else int(a['values'][p1]) for a in data]
-    s2 = ['' if a['values'][p2] in ['n/a', 'n/m'] else int(a['values'][p2]) for a in data]
+    s1 = ['' if a['values'][args[0]] in ['n/a', 'n/m'] else int(a['values'][args[0]]) for a in data]
+    try:
+        s2 = ['' if a['values'][args[1]] in ['n/a', 'n/m'] else int(a['values'][args[1]]) for a in data]
+    except IndexError:
+        s2 = None
     return {'times': times, 's1': s1, 's2': s2}
 
 
@@ -23,6 +29,20 @@ def make_chart(site, days, pollutant_1, pollutant_2, chartID='chart_ID', chart_t
                chart_width=800):
     chart = {"renderTo": chartID, "type": chart_type, "height": chart_height, "width": chart_width}
     data = chart_data(site, days, pollutant_1,  pollutant_2)
+    series = [{"name": pollutant_1, "data": data['s1']}, {"name": pollutant_2, "data": data['s2']}]
+    title = {"text": 'Recent air pollution levels at {}'.format(site)}
+    xAxis = {"title": {"text": 'Time (GMT)'}, "categories": data['times']}
+    yAxis = {"title": {"text": 'Concentration (ug/m-3)'}}
+    return render_template('chart.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis,
+                           yAxis=yAxis)
+
+
+@app.route('<site_1>/<site_2>/<int:days>/<pollutant>')
+def plot_two_sites(site_1, site_2, days, pollutant, chartID='chart_ID', chart_type='line', chart_height=550,
+               chart_width=800):
+    chart = {"renderTo": chartID, "type": chart_type, "height": chart_height, "width": chart_width}
+    data_1 = chart_data(site_1, days, pollutant)
+    data_2 = chart_data(site_2, days, pollutant)
     series = [{"name": pollutant_1, "data": data['s1']}, {"name": pollutant_2, "data": data['s2']}]
     title = {"text": 'Recent air pollution levels at {}'.format(site)}
     xAxis = {"title": {"text": 'Time (GMT)'}, "categories": data['times']}
