615319fd0dd43f84bc4cdc12da232db832f4dcea
Paul Davis <pjdavis@gmx.com>
2017-12-20 22:37:30 +0100

    update example app

--- Flask-RESTPlus_Marshmallow/marshmallow_intro.py
+++ Flask-RESTPlus_Marshmallow/marshmallow_intro.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, pprint, post_load
 
 class Person():
     def __init__(self, name, age):
@@ -12,13 +12,25 @@ class PersonSchema(Schema):
     name = fields.String()
     age = fields.Integer()
 
+
+    #person object defined below is instantiated by this Schema
+    @post_load
+    def create_person(self, data):
+        return Person(**data)
+
 input_dict = {}
 
 input_dict['name'] = input('What is your name?')
 input_dict['age'] = input('What is your age?')
 
-person = Person(name=input_dict['name'], age=input_dict['age'])
+#person = Person(name=input_dict['name'], age=input_dict['age'])
 
 # print the string representation of the person object:
 #print(person)
+
+schema = PersonSchema()
+#result = schema.dump(person)
+#result = schema.load(input_dict)
+
+pprint(schema.data)
 
