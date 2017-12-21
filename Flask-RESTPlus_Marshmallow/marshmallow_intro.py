ae51d81739e0e0144fb591d7051e590f54f7f825
Paul Davis <pjdavis@gmx.com>
2017-12-21 16:39:49 +0100

    create socketio dir

--- Flask-RESTPlus_Marshmallow/marshmallow_intro.py
+++ Flask-RESTPlus_Marshmallow/marshmallow_intro.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, pprint, post_load, ValidationError
+from marshmallow import Schema, fields, pprint, post_load, ValidationError, validates
 
 class Person():
     def __init__(self, name, age, email):
@@ -9,14 +9,9 @@ class Person():
     def __repr__(self):
         return  '{} is {} years old'.format(self.name, self.age)
 
-def validate_age(age):
-    if age < 25:
-        #return False
-        raise ValidationError('That is too young')
-
 class PersonSchema(Schema):
     name = fields.String()
-    age = fields.Integer(validate=validate_age)
+    age = fields.Integer()
     email = fields.Email()
     location = fields.String(required=True)
 
@@ -26,12 +21,18 @@ class PersonSchema(Schema):
     def create_person(self, data):
         return Person(**data)
 
+    @validates('age')
+    def validate_age(self, age):
+        if age < 25:
+            # return False
+            raise ValidationError('That is too young')
+
 input_dict = {}
 
 input_dict['name'] = input('What is your name?')
 input_dict['age'] = input('What is your age?')
 input_dict['email'] = input('What is your email? ')
-
+input_dict['location'] = input('What is your location?')
 
 #person = Person(name=input_dict['name'], age=input_dict['age'])
 
