c408438f7d0e4339af6cc243fd5d573677df4660
Paul Davis <pjdavis@gmx.com>
2017-12-22 09:55:34 +0100

    update chat app

--- Flask-SocketIO/app.py
+++ Flask-SocketIO/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -14,6 +14,7 @@ def index():
 @socketio.on('message')
 def receive_message(message):
     print ('############: {}'.format(message))
+    send('This is a message from the flask app server')
 
 if __name__ == '__main__':
     #socketio is just a wrapper around the flask app server
--- Flask-SocketIO/static/script.js
+++ Flask-SocketIO/static/script.js
@@ -5,7 +5,9 @@ var socket = io.connect('http://127.0.0.1:5000');
 // callback function to send a message
 socket.on('connect', function() {
     socket.send('I am now connected!');
+
+    socket.on('message', function(msg) {
+        alert(msg);
+    });
 });
-
-
 
