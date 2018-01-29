1fdcc04a6daae2cfd1bb507e0ff291c2759279c0
Paul Davis <pjdavis@gmx.com>
2018-01-29 09:58:22 +0100

    rename folders

--- product_catalog/my_app/__init__.py
+++ product_catalog/my_app/__init__.py
@@ -1,12 +1,15 @@
-from flask import Flask 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Manager, Migrate, MigrateCommand
 
-app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['WTF_CSRF_SECRET_KEY'] = 'random key for form'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from my_app.catalog.views import catalog
-app.register_blueprint(catalog)
-
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+import my_app.catalog.views
 db.create_all()
--- product_catalog/my_app/catalog/views.py
+++ product_catalog/my_app/catalog/views.py
@@ -1,3 +1,16 @@
+from flask import request, jsonify, Blueprint, render_template, flash, redirect, url_for
+from my_app import db
+from my_app.catalog.models import Product, Category, ProductForm
+
+
+catalog = Blueprint('catalog', __name__)
+
+
+@catalog.route('/')
+@catalog.route('/home')
+def home():
+    return "Welcome to the Catalog Home."
+
 
 @catalog.route('/products')
 def products():
@@ -6,45 +19,60 @@ def products():
     for product in products:
         res[product.id] = {
             'name': product.name,
-            'price': str(product.price)
+            'price': str(product.price),
+            'category': product.category.name
                             }
     return jsonify(res)
 
+@catalog.route('/product-create', methods=['GET', 'POST'])
+def create_product():
+    form = ProductForm(request.form)
 
+    categories = [(c.id, c.name) for c in Category.query.all()]
+    form.category.choices = categories
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        price = request.form.get('price')
+        category = Category.query.get_or_404(request.form.get('category'))
+        product = Product(name, price, category)
+        db.session.add(product)
+        db.session.commit()
+        flash('The product %s has been created' % name, 'success')
+        return redirect(url_for('catalog.product', id=product.id))
+    return render_template('product_create.html', form=form)
+
+
+@catalog.route('/category-create', methods=['POST',])
+def create_category():
+    name = request.form.get('name')
+    category = Category(name)
+    db.session.add(category)
+    db.session.commit()
+    return 'Category created.'
+
+
+"""
 @catalog.route('/product-create', methods=['POST',])
 def create_product():
     name = request.form.get('name')
     price = request.form.get('price')
-    product = Product(name, price)
+    categ_name = request.form.get('category')
+    category = Category.query.filter_by(name=categ_name).first()
+    if not category:
+        category = Category(categ_name)
+    product = Product(name, price, category)
     db.session.add(product)
     db.session.commit()
     return 'Product created.'
+    """
 
-
-class ProductForm(FlaskForm):
-    name = StringField('name')
-    price = IntegerField('price')
-
-
-@catalog.route('/form', methods=['GET', 'POST'])
-def product_form():
-    form = ProductForm()
-
-    if form.validate_on_submit():
-        return 'The username is {}. The password is {}'.format (form.username.data, form.password.data)
-    return render_template('form.html', form=form)
-
-
-class LoginForm(FlaskForm):
-    username = StringField('username')
-    password = PasswordField('password')
-
-
-@catalog.route('/form', methods=['GET', 'POST'])
-def form():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        return 'The username is {}. The password is {}'.format (form.username.data, form.password.data)
-    return render_template('form.html', form=form)
-
+@catalog.route('/categories')
+def categories():
+    categories = Category.query.all()
+    res = {}
+    for category in categories:
+        res[category.id] = {'name': category.name}
+    for product in category.products:
+        res[category.id]['products'] = {'id': product.id, 'name': product.name, 'price': product.price}
+    return jsonify(res)
