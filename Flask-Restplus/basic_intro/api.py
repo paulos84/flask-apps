272d797331e11f3ff4fe0ba0b75a1181d3e60fd2
Paul Davis <pjdavis@gmx.com>
2018-02-20 10:28:07 +0100

    update ma cities

--- Flask-Restplus/basic_intro/api.py
+++ Flask-Restplus/basic_intro/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint
-from flask_restplus import Api, Resource, fields
-from marshmallow import Schema, Fields
+from flask_restplus import Api, Resource
+from marshmallow import Schema, fields, post_load
 
 app = Flask(__name__)
 blueprint = Blueprint('api', __name__, url_prefix='/api')
@@ -12,10 +12,27 @@ app.config['SWAGGER_UI_JSONEDITOR'] = True
 #city = api.model('City', {'city': fields.String('The city')})  # 'id': fields.Integer
 
 class City(object):
-    def __init__(self, ):
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def __repr__(self):
+        return '{} is the city name. {} is the city population'.format(self.name, self.population)
+
+    @post_load
+    def create_city(self, data):
+        # validation?
+        return City(**data)
+
+class LanguageSchema(Schema):
+    name = fields.String()
+    population = fields.Integer()
+
+
 
 cities = []
-tokyo = {'city': 'Tokyo'}
+#tokyo = {'city': 'Tokyo'}
+tokyo = City(name='Tokyo', population=18000000)
 cities.append(tokyo)
 
 
@@ -23,7 +40,7 @@ cities.append(tokyo)
 class City(Resource):
 
     # The marshal_with() decorator will apply the transformation described by model which uses the fields module
-    @api.marshal_with(city, envelope='Cities')
+
     def get(self):
         return cities  # return an iterable
 
